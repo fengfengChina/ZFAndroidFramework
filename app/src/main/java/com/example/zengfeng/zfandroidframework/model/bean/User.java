d8a1ef7ccafe95cbf3befc6f4e15181a2dcd3544
zengfeng <305737657@qq.com>
2016-05-11 09:25:42 +0800

    model service

---
+++ app/src/main/java/com/example/zengfeng/zfandroidframework/model/bean/User.java
@@ -0,0 +1,8 @@
+package com.example.zengfeng.zfandroidframework.model.bean;
+
+/**
+ * app 登陆后台保存的class
+ * Created by zengfeng on 5/11/16.
+ */
+public class User {
+}
---
+++ app/src/main/java/com/example/zengfeng/zfandroidframework/model/bean/remote/Data.java
@@ -0,0 +1,8 @@
+package com.example.zengfeng.zfandroidframework.model.bean.remote;
+
+/**
+ * 远端数据实体类
+ * Created by zengfeng on 5/11/16.
+ */
+public class Data {
+}
