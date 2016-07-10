4fdf4d4c97adea52f06bd81f99b5b4b6bc1411ac
ZengFeng <305737657@qq.com>
2016-07-10 17:57:07 +0800

    some base class

--- app/src/main/java/com/zero/android/common/BasePresent.java
+++ app/src/main/java/com/zero/android/common/BasePresent.java
@@ -4,6 +4,13 @@ package com.zero.android.common;
  * Created by zengfeng on 16/7/6.
  */
 public interface BasePresent  {
+    /**
+     * 订阅
+     */
+    void subscribe();
 
-    public void init();
+    /**
+     * 取消订阅
+     */
+    void unsubscribe();
 }
