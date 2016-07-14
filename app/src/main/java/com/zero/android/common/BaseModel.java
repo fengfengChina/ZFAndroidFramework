c0e93a725180c477949b75dfcc524a1487f5f57f
ZengFeng <305737657@qq.com>
2016-07-14 09:52:56 +0800

    add base model

---
+++ app/src/main/java/com/zero/android/common/BaseModel.java
@@ -0,0 +1,10 @@
+package com.zero.android.common;
+
+/**
+ * Created by zengfeng on 16/7/14.
+ */
+public class BaseModel{
+    public static String BASE_URL = "http://120.24.240.62:8090/";
+
+
+}
--- app/src/main/java/com/zero/android/model/LoginModel.java
+++ app/src/main/java/com/zero/android/model/LoginModel.java
@@ -1,29 +1,48 @@
 package com.zero.android.model;
 
-import android.os.Handler;
-import android.text.TextUtils;
+import android.util.Log;
+
+import com.zero.android.Api;
+import com.zero.android.common.BaseModel;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
  * Created by zengfeng on 16/7/6.
  */
-public class LoginModel {
+public class LoginModel extends BaseModel{
 
     public void login(String userName, String password, OnLoginListner listner){
-        new Handler().postDelayed(()-> {
-                boolean error = false;
-                if (TextUtils.isEmpty(userName)){
-                    listner.onUsernameError();
-                    error = true;
+        Retrofit           retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        Api                api      = retrofit.create(Api.class);
+        Call<ResponseBody> call     =  api.login(userName,password);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                try {
+                    Log.i("wxl", "response=" + response.body().string());
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                if (TextUtils.isEmpty(password)){
-                    listner.onPasswordError();
-                    error = true;
-                }
-                if (!error) {
-                    listner.onSuccess();
-                }
-        }, 2000);
+                listner.onSuccess();
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.i("wxl", "onFailure=" + t.getMessage());
+                listner.onPasswordError();
+                listner.onUsernameError();
+            }
+        });
+
     }
 
 }
