0fe0063d664c9fef66b8b2aec64087b84044620f
ZengFeng <305737657@qq.com>
2016-07-23 00:31:11 +0800

    intent

--- app/src/main/java/com/zero/android/common/utils/IntentUtils.java
+++ app/src/main/java/com/zero/android/common/utils/IntentUtils.java
@@ -1,8 +1,14 @@
 package com.zero.android.common.utils;
 
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by zf on 2016/2/4.
  */
 public class IntentUtils {
-
+    public static void goListActivity(Context context,Class clazz){
+        Intent intent= new Intent(context,clazz);
+        context.startActivity(intent);
+    }
 }
